d3040947d230a9faa6996852287c3eed38f49924
Ye Dong <1187231197@qq.com>
2020-12-07 01:49:26 -0500

    add ill

--- vaccine_system.py
+++ vaccine_system.py
@@ -1,7 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-class citizen(object) : 
+class Citizen(object) : 
+    """ Citizen Agent """
     def __init__(self, info : dict) : 
         self.age = info["age"] 
         self.gender = info["gender"]
@@ -16,6 +17,7 @@ class citizen(object) :
         pass  
 
 class Government(object) : 
+    """ Government Agent """
     def __init__(self, policy : str) : 
         self.policy = policy 
         self.citizens = []    
@@ -46,6 +48,8 @@ class vaccine_system(object) :
         self.days = days 
         self.r_nau = r_nau 
         self.gov = gov 
+        self.gammas = np.random.poisson(5, len(self.infected_citizens))
+        self.ills = []
 
     def gen_info(self) :
         """ generate information for virtual citizens """
@@ -65,7 +69,7 @@ class vaccine_system(object) :
 
         for i in range(citizen_num) : 
             info = self.gen_info()
-            citizen_ = citizen(info)
+            citizen_ = Citizen(info)
             
             if info["infected"] : 
                 infected_citizens.append(citizen_)
@@ -76,7 +80,7 @@ class vaccine_system(object) :
 
         return citizens, infected_citizens, uninfected_citizens
 
-    def recovery(self, re, method="resolve") : 
+    def recovery(self, re, ill_rate=0.1, method="resolve") : 
         """ simulate the recovery """
 
         if method == "ratio" :  
@@ -89,20 +93,44 @@ class vaccine_system(object) :
                 if citizen_.gamma == 0 : 
                     recovery_num += 1 
                     new_recovery_idx.append(idx)
+                    citizen_.gamma = np.random.poisson(5)
 
         self.infected_citizens = np.array(self.infected_citizens)
         
+        # get the ill index from the new recovered 
+        if len(new_recovery_idx) > 0 : 
+            ill_num = int(ill_rate * len(new_recovery_idx))
+            ill_idx = list(np.random.choice(new_recovery_idx, ill_num, replace=False))
+        else : 
+            ill_idx = []
+
+        # filter out the new recovered 
         mask = np.ones(len(self.infected_citizens), dtype=bool)
         mask[new_recovery_idx] = False
         new_recovery_citizen = self.infected_citizens[~mask]
+        
+        # filter out the illed 
+        mask[new_recovery_idx] = True 
+        mask[ill_idx] = False 
+        ill_new_recovery = self.infected_citizens[~mask]
 
-        self.requests.extend(new_recovery_citizen)
+        # filter out the healthy new recovered 
+        mask[new_recovery_idx] = False 
+        mask[ill_idx] = True 
+        healthy_new_recovery = self.infected_citizens[~mask]
+
+        # filter out the left infected 
+        mask[ill_idx] = False 
         self.infected_citizens = self.infected_citizens[mask] 
-        self.uninfected_citizens.extend(new_recovery_citizen)
         self.infected_citizens = list(self.infected_citizens)
 
-        #print("recovery", len(new_recovery_citizen))
-        #print(len(self.infected_citizens), len(self.uninfected_citizens), len(self.requests))
+        # requests and unifected add back the healty recovered 
+        self.requests.extend(healthy_new_recovery)
+        self.uninfected_citizens.extend(new_recovery_citizen)
+
+        # record ill people 
+        self.ills.extend(ill_new_recovery)
+        #print(len(self.ills), len(new_recovery_idx)) 
 
     def infection_spread(self, r_nau, infectious_period=5, random_mode=True) :
         """ simulate the infection """
@@ -116,8 +144,15 @@ class vaccine_system(object) :
                 if citizen_.gamma > 0 : 
                     new_infected_num += r_nau / infectious_period
                     citizen_.gamma -= 1  
-                else : 
-                    citizen_.gamma = np.random.poisson(5)
+
+            # old_gammas = self.gammas[self.gammas > 0]  
+            # new_infected_num = np.sum(old_gammas) * (r_nau / infectious_period)
+
+            # old_gammas -= 1 
+            # new_gammas_len = len(self.gammas[self.gammas == 0])
+            # new_gammas = np.random.poisson(infectious_period, new_gammas_len)
+            # self.gammas = np.append(old_gammas, new_gammas)
+
         else : 
             new_infected_num = len(self.infected_citizens) * r_nau / infectious_period 
 
@@ -141,10 +176,11 @@ class vaccine_system(object) :
 
     def update(self) : 
         """" implements recovery -> infection -> vaccine injection """
-        # infection spread 
+        # recover 
         self.recovery(0.1)
         #print("recovery", "I : ", len(self.infected_citizens), "U : ", len(self.uninfected_citizens), "R : ", len(self.requests))
 
+        # infection spread 
         noise = np.random.normal()
         noise = 1 if noise < -self.r_nau else noise  
         noisy_r_nau = self.r_nau + noise 
@@ -181,6 +217,7 @@ class vaccine_system(object) :
         uninfected_num = []
         requests_num = []
         anti_body_num = []
+        ill_num = []
 
         for i in range(self.days) : 
             self.update()
@@ -188,8 +225,9 @@ class vaccine_system(object) :
             uninfected_num.append(len(self.uninfected_citizens))
             requests_num.append(len(self.requests))
             anti_body_num.append(len(self.uninfected_citizens) - len(self.requests))
+            ill_num.append(len(self.ills))
         
-        return infected_num, uninfected_num, requests_num, anti_body_num
+        return infected_num, uninfected_num, requests_num, anti_body_num, ill_num
 
     def reset(self) : 
         """ reset all parameters """
@@ -200,6 +238,7 @@ class vaccine_system(object) :
         self.requests = list(self.uninfected_citizens)
         np.random.shuffle(self.requests)
         self.vaccinated = []
+        self.ills = []
         self.gov.reset() 
         
     def simulate(self, sim_num = 1) : 
@@ -208,17 +247,18 @@ class vaccine_system(object) :
         uninfected_nums = []
         requests_nums = []
         anti_body_nums = []
+        ill_nums = []
 
         for i in range(sim_num) : 
-            infected_num, uninfected_num, requests_num, anti_body_num = self.start() 
+            infected_num, uninfected_num, requests_num, anti_body_num, ill_num = self.start() 
             infected_nums.append(infected_num)
             uninfected_nums.append(uninfected_num)
             requests_nums.append(requests_num)
             anti_body_nums.append(anti_body_num)
+            ill_nums.append(ill_num)
             self.reset() 
-        
-        #print(infected_nums)
-        return infected_nums, uninfected_nums, requests_nums, anti_body_nums
+
+        return infected_nums, uninfected_nums, requests_nums, anti_body_nums, ill_nums 
 
 def collect_stats(data, axis) : 
     """ collect means and std """
@@ -230,10 +270,14 @@ def collect_stats(data, axis) :
     return mean, std 
 
 def main_sim(parameters : dict) :
-    """ run all simulations and plotting time-varying graphs """
+    """ run all simulations and plotting time-varying graphs 
+        parameters : make sure that one and only one of the keys correspond to list values, mainly for params comparison 
+    """
     params_key = parameters.keys()
+    days = parameters["days"] if parameters["days"] else 7 
     temp_parameters = parameters 
 
+    # select the params values for comparison 
     for param in params_key : 
         if type(parameters[param]) is list and len(parameters[param]) > 1 : 
             temp_param_values = parameters[param]
@@ -242,20 +286,21 @@ def main_sim(parameters : dict) :
             temp_params = temp_parameters
             break 
 
-    fig, axes = plt.subplots(2, len(temp_param_values))
+    fig, axes = plt.subplots(2, len(temp_param_values), figsize=(12, 8))
     
+    # compare param values 
     for idx, param_value in enumerate(temp_param_values) : 
         temp_params[temp_param_key] = param_value
         env = vaccine_system(**temp_params)
-        infected_nums, uninfected_nums, requests_nums, anti_body_nums = env.simulate(sim_num=100)
+        infected_nums, uninfected_nums, requests_nums, anti_body_nums, ill_nums = env.simulate(sim_num=100)
 
         datas = [infected_nums, uninfected_nums]
         labels = ["infected_num", "uninfected_num"]
-        plotting(datas, labels, days=7, ax=axes[0][idx], title=(temp_param_key, param_value))
+        plotting(datas, labels, days, ax=axes[0][idx], title=(temp_param_key, param_value))
 
-        datas = [requests_nums, anti_body_nums]
-        labels = ["request_num", "anti_body_num"]
-        plotting(datas, labels, days=7, ax=axes[1][idx], title=(temp_param_key, param_value))
+        datas = [requests_nums, anti_body_nums, ill_nums]
+        labels = ["request_num", "anti_body_num", "seriously_ill_num"]
+        plotting(datas, labels, days, ax=axes[1][idx], title=(temp_param_key, param_value))
 
     plt.show()
 
